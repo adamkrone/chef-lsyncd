bf34e917427d78fc567883d59aa34e48e69989ee
Adam Krone <krone.adam@gmail.com>
2015-12-11 11:18:08 -0600

    v0.2.0 release

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'adam.krone@thirdwavellc.com'
 license          'Apache v2.0'
 description      'Installs/Configures lsyncd'
 long_description 'Installs/Configures lsyncd'
-version          '0.1.0'
+version          '0.2.0'
 
